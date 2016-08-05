e60e1e9284e482a6db5074ae07f75f62b112518b
Iskender Piyale-Sheard <ipiyale@gmail.com>
2016-08-05 16:17:08 -0400

    New Website

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,108 +1,106 @@
 /*
-Theme Name: IAMX
-Author: Trendy Theme
-Author URL: trendytheme.net
-*/
-
-/*
-    = Preloader
-    = Animated scrolling / Scroll Up
-    = Full Screen Slider
-    = Sticky Menu
-    = Back To Top
-    = Countup
-    = Progress Bar
-    = More skill
-    = Shuffle
-    = Magnific Popup
-    = Vidio auto play
-    = Fit Vids
-    = Google Map
-
+Template Name: Smartex
+Author: TrendyTheme
+Version: 1.0
 */
 
 jQuery(function ($) {
 
     'use strict';
 
-    /* ---------------------------------------------- /*
-     * Preloader
-    /* ---------------------------------------------- */
+    /* ======= Blank Wrapper ======= */
+    (function () {
 
-    $(window).ready(function() {
-        $('#pre-status').fadeOut();
-        $('#tt-preloader').delay(350).fadeOut('slow');
+        // content here...
+
+    }());
+
+
+    /* ======= Preloader ======= */
+    (function () {
+        $('#status').fadeOut();
+        $('#preloader').delay(200).fadeOut('slow');
+    }());
+
+
+
+    //jQuery to collapse the navbar on scroll
+    $(window).scroll(function() {
+        if ($(".navbar").offset().top > 50) {
+            $(".navbar-fixed-top").addClass("sticky");
+        } else {
+            $(".navbar-fixed-top").removeClass("sticky");
+        }
+    });
+
+
+    /* === jQuery for page scrolling feature - requires jQuery Easing plugin === */
+    (function () {
+        $('a.page-scroll').on('click', function(event) {
+            var $anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $($anchor.attr('href')).offset().top
+            }, 1500, 'easeInOutExpo');
+            event.preventDefault();
+        });
+    }());
+    
+
+    
+    /* === Collaps mobile menu when click on anchor === */
+    (function () {
+	    $('.navbar-custom a.page-scroll').on('click', function(event) {
+	        $('.navbar-custom').find('.navbar-collapse').removeClass('in');
+	    });
+    }());
+    
+
+
+    /* ======= Full Screen Background ======= */
+    
+    $(".tt-fullHeight").height($(window).height());
+    $(window).resize(function(){
+        $(".tt-fullHeight").height($(window).height());
+    });
+
+
+
+    /* === magnificPopup === */
+
+    $('.tt-lightbox').magnificPopup({
+        type: 'image',
+        mainClass: 'mfp-fade',
+        removalDelay: 160,
+        fixedContentPos: false
+        // other options
+    });
+
+
+    /* === Youtube Video Script === */
+    if ($('.player').length > 0) {
+
+        jQuery(".player").mb_YTPlayer();
+        
+    }
+
+
+    /* === Progress Bar === */
+    $('.progress').on('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $.each($('div.progress-bar'),function(){
+                $(this).css('width', $(this).attr('aria-valuenow')+'%');
+            });
+            $(this).off('inview');
+        }
     });
 
 
 
 
-    // -------------------------------------------------------------
-    // Animated scrolling / Scroll Up
-    // -------------------------------------------------------------
-
-    (function () {
-        $('a[href*=#]').bind("click", function(e){
-            var anchor = $(this);
-            $('html, body').stop().animate({
-                scrollTop: $(anchor.attr('href')).offset().top
-            }, 1000);
-            e.preventDefault();
-        });
-    }());
 
 
-
-    // -------------------------------------------------------------
-    // Full Screen Slider
-    // -------------------------------------------------------------
-    (function () {
-        $(".tt-fullHeight").height($(window).height());
-
-        $(window).resize(function(){
-            $(".tt-fullHeight").height($(window).height());
-        });
-
-    }());
-
-
-    // -------------------------------------------------------------
-    // Sticky Menu
-    // -------------------------------------------------------------
-
-    (function () {
-        $('.header').sticky({
-            topSpacing: 0
-        });
-
-        $('body').scrollspy({
-            target: '.navbar-custom',
-            offset: 70
-        })
-    }());
-
-
-
-
-    // -------------------------------------------------------------
-    // Back To Top
-    // -------------------------------------------------------------
-
-    (function () {
-        $(window).scroll(function() {
-            if ($(this).scrollTop() > 100) {
-                $('.scroll-up').fadeIn();
-            } else {
-                $('.scroll-up').fadeOut();
-            }
-        });
-    }());
-
-
-    // -------------------------------------------------------------
-    // Countup
-    // -------------------------------------------------------------
-    $('.count-wrap').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
+    /* === Counter === */
+    $('.counter-section').on('inview', function(event, visible, visiblePartX, visiblePartY) {
         if (visible) {
             $(this).find('.timer').each(function () {
                 var $this = $(this);
@@ -114,184 +112,83 @@ jQuery(function ($) {
                     }
                 });
             });
-            $(this).unbind('inview');
+            $(this).off('inview');
         }
     });
 
 
-    // -------------------------------------------------------------
-    // Progress Bar
-    // -------------------------------------------------------------
- 
-    $('.skill-progress').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-        if (visible) {
-            $.each($('div.progress-bar'),function(){
-                $(this).css('width', $(this).attr('aria-valuenow')+'%');
-            });
-            $(this).unbind('inview');
-        }
-    });
-    
-    // -------------------------------------------------------------
-    // More skill
-    // -------------------------------------------------------------
-    $('.more-skill').bind('inview', function(event, visible, visiblePartX, visiblePartY) {
-        if (visible) {
-            $('.chart').easyPieChart({
-                //your configuration goes here
-                easing: 'easeOut',
-                delay: 3000,
-                barColor:'#68c3a3',
-                trackColor:'rgba(255,255,255,0.2)',
-                scaleColor: false,
-                lineWidth: 8,
-                size: 140,
-                animate: 2000,
-                onStep: function(from, to, percent) {
-                    this.el.children[0].innerHTML = Math.round(percent);
-                }
-
-            });
-            $(this).unbind('inview');
-        }
-    });
 
 
-    // -------------------------------------------------------------
-    // Shuffle
-    // -------------------------------------------------------------
-
+    /* ======= Recent Post Carousel ======= */
     (function () {
 
-        var $grid = $('#grid');
+      var owl = $(".recent-post-carousel");
+     
+      owl.owlCarousel({
+          items : 4, //4 items above 1000px browser width
+          itemsDesktop : [1024,4], //4 items between 1000px and 901px
+          itemsDesktopSmall : [900,2], // betweem 900px and 601px
+          itemsTablet: [600,2], //2 items between 600 and 480
+          itemsMobile : [479,1], //1 item between 480 and 0
+          pagination : false, // Show pagination
+          autoPlay: true
+      });
 
-        $grid.shuffle({
-            itemSelector: '.portfolio-item'
-        });
 
-        /* reshuffle when user clicks a filter item */
-        $('#filter a').click(function (e) {
-            e.preventDefault();
-
-            // set active class
-            $('#filter a').removeClass('active');
-            $(this).addClass('active');
-
-            // get group name from clicked item
-            var groupName = $(this).attr('data-group');
-
-            // reshuffle grid
-            $grid.shuffle('shuffle', groupName );
-        });
+      // Custom Navigation Events
+      $(".btn-next").on('click', function(){
+        owl.trigger('owl.next');
+      })
+      $(".btn-prev").on('click', function(){
+        owl.trigger('owl.prev');
+      })
 
 
     }());
 
 
-    // -------------------------------------------------------------
-    // Magnific Popup
-    // -------------------------------------------------------------
-
+    /* ======= Partner Carousel ======= */
     (function () {
-      $('.image-link').magnificPopup({
 
-        gallery: {
-          enabled: true
-        },
-        removalDelay: 300, // Delay in milliseconds before popup is removed
-        mainClass: 'mfp-with-zoom', // this class is for CSS animation below
-        type:'image'
+      var owl = $(".partner-carousel");
+     
+      owl.owlCarousel({
+          items : 5, //5 items above 1000px browser width
+          itemsDesktop : [1024,4], //4 items between 1000px and 901px
+          itemsDesktopSmall : [900,3], // betweem 900px and 601px
+          itemsTablet: [600,2], //2 items between 600 and 480
+          itemsMobile : [479,1], //1 item between 480 and 0
+          pagination : false, // Show pagination
+          autoPlay: true
       });
 
     }());
 
 
 
-    (function () {
-        $('.popup-video').magnificPopup({
-            disableOn: 700,
-            type: 'iframe',
-            mainClass: 'mfp-with-zoom',
-            removalDelay: 300,
-            preloader: false,
-            fixedContentPos: false
-        });
-    }());
 
-
-
-
-
-    // -------------------------------------------------------------
-    // Fit Vids
-    // -------------------------------------------------------------
-    (function () {
-        $(".video-container").fitVids();
-    }());
-
-
-
-    // -------------------------------------------------------------
-    // Vidio auto play
-    // -------------------------------------------------------------
-    (function () {
-    
-    /* Vimeo API: http://developer.vimeo.com/player/js-api */
-    
-        var iframe = document.getElementById('nofocusvideo');
-        // $f == Froogaloop
-        var player = $f(iframe);
-
-        $('.modal').on('hidden.bs.modal', function () {
-        player.api('pause');
-        })
-
-        $('.modal').on('shown.bs.modal', function () {
-        player.api('play');
-        })
-    }());
-
-
-
-
-    // -------------------------------------------------------------
-    // STELLAR FOR BACKGROUND SCROLLING
-    // -------------------------------------------------------------
-
-    $(window).load(function() {
-
-        if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-         
-        }else {
-            $.stellar({
-                horizontalScrolling: false,
-                responsive: true
-            });
+    /* ======= BlackAndWhite Script ======= */
+    $('.bwWrapper').BlackAndWhite({
+        hoverEffect : true, // default true
+        // set the path to BnWWorker.js for a superfast implementation
+        webworkerPath : false,
+        // to invert the hover effect
+        invertHoverEffect: false,
+        // this option works only on the modern browsers ( on IE lower than 9 it remains always 1)
+        intensity:1,
+        speed: { //this property could also be just speed: value for both fadeIn and fadeOut
+            fadeIn: 200, // 200ms for fadeIn animations
+            fadeOut: 800 // 800ms for fadeOut animations
+        },
+        onImageReady:function(img) {
+            // this callback gets executed anytime an image is converted
         }
-
     });
 
 
-    // -------------------------------------------------------------
-    // WOW JS
-    // -------------------------------------------------------------
-
-    (function () {
-
-        new WOW({
-
-            mobile:  false
-
-        }).init();
-
-    }());
 
 
-
-    // -------------------------------------------------------------
-    // Contact Form
-    // -------------------------------------------------------------
-
+    /* ======= Contact Form ======= */
     $('#contactForm').on('submit',function(e){
 
         e.preventDefault();
@@ -322,74 +219,64 @@ jQuery(function ($) {
 
 
 
-    // -------------------------------------------------------------
-    // Google Map
-    // -------------------------------------------------------------
 
-    (function () {
-        var myLatlng = new google.maps.LatLng(41.372641, -74.687387);
 
-            var styles = [
-                {
-                    featureType: "landscape",
-                    stylers: [
-                        { color: '#f7f7f7' }
-                    ]
-                },{
-                    featureType: "natural",
-                    stylers: [
-                        { hue: '#00ffe6' }
-                    ]
-                },{
-                    featureType: "road",
-                    stylers: [
-                        { hue: '#fff' },
-                        { saturation: -70 }
-                    ]
-                },{
-                    featureType: "building",
-                    elementType: "labels",
-                    stylers: [
-                        { hue: '' }
-                    ]
-                },{
-                    featureType: "poi", //points of interest
-                    stylers: [
-                        { hue: '' }
-                    ]
-                }
-            ];
+    /* ======= GOOGLE MAP ======= */
+    if ($('#myMap').length > 0) {
+        //set your google maps parameters
+        var $latitude = 51.558588, //If you unable to find latitude and longitude of your address. Please visit http://www.latlong.net/convert-address-to-lat-long.html you can easily generate.
+            $longitude = -0.279540,
+            $map_zoom = 16 /* ZOOM SETTING */
 
-            var mapOptions = {
-                zoom: 15,
-                scrollwheel: false,
-                center: myLatlng,
-                mapTypeId: google.maps.MapTypeId.ROADMAP,
-                disableDefaultUI: true,
-                styles: styles
-            }
-            var map = new google.maps.Map(document.getElementById('mapCanvas'), mapOptions);
+        //google map custom marker icon 
+        var $marker_url = 'assets/images/pin.png';
 
-            var marker = new google.maps.Marker({
-                position: myLatlng,
-                map: map,
-                animation: google.maps.Animation.DROP,
-                title: 'Hello World!'
-            });
+        //we define here the style of the map
+        var style = [{
+            "stylers": [{
+                "hue": "#000"
+            }, {
+                "saturation": -100
+            }, {
+                "gamma": 2.15
+            }, {
+                "lightness": 0
+            }]
+        }];
 
-            var contentString = '' +
-                    '' +
-                    '';
+        //set google map options
+        var map_options = {
+            center: new google.maps.LatLng($latitude, $longitude),
+            zoom: $map_zoom,
+            panControl: true,
+            zoomControl: true,
+            mapTypeControl: true,
+            streetViewControl: true,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            scrollwheel: false,
+            styles: style
+        }
+        //inizialize the map
+        var map = new google.maps.Map(document.getElementById('myMap'), map_options);
+        //add a custom marker to the map                
+        var marker = new google.maps.Marker({
+            position: new google.maps.LatLng($latitude, $longitude),
+            map: map,
+            visible: true,
+            icon: $marker_url
+        });
 
-            var infowindow = new google.maps.InfoWindow({
-                content: contentString
-            });
+        var contentString = '<div id="mapcontent">' + '<p>Resume, Engineers Way, United Kingdom.</p></div>';
+        var infowindow = new google.maps.InfoWindow({
+            maxWidth: 320,
+            content: contentString
+        });
 
-            google.maps.event.addListener(marker, 'click', function () {
-                infowindow.open(map, marker);
-            });
-
-    }());
+        google.maps.event.addListener(marker, 'click', function() {
+            infowindow.open(map, marker);
+        });
+       
+    }
 
 
 });
@@ -398,3 +285,34 @@ jQuery(function ($) {
 
 
 
+$(window).load(function() {
+
+    "use strict";
+
+    /* ======= shuffle js ======= */
+    if ($('#portfolio-grid').length > 0) {
+        /* initialize shuffle plugin */
+        var $grid = $('#portfolio-grid');
+
+        $grid.shuffle({
+            itemSelector: '.portfolio-item' // the selector for the items in the grid
+        });
+
+        /* reshuffle when user clicks a filter item */
+        $('#filter li').on('click', function (e) {
+            e.preventDefault();
+
+            // set active class
+            $('#filter li').removeClass('active');
+            $(this).addClass('active');
+
+            // get group name from clicked item
+            var groupName = $(this).attr('data-group');
+
+            // reshuffle grid
+            $grid.shuffle('shuffle', groupName );
+        });
+    }
+
+
+});
